21e45aced15f664a7f0e7f8c9a7a9aa600a472df
JibbyKT <89538905+JibbyKT@users.noreply.github.com>
2022-06-06 11:01:16 +0800

    Weather icons and basic HTML layout

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello~");
+
+const api_key = "0d32655573f280eaa5d85c7e6fa638a5";
+
+let;
+
+/*
+Current forecast - https://openweathermap.org/current#data
+5 day forecast - https://openweathermap.org/forecast5
+*/
