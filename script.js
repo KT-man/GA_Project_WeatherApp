9c2c63dd533febe51e5545eecfa03cc669e5bc5f
JibbyKT <89538905+JibbyKT@users.noreply.github.com>
2022-06-09 13:54:23 +0800

    OK FINAL VERSION BEFORE REFACTOR

--- script.js
+++ script.js
@@ -66,122 +66,6 @@ const wiToOWM = {
   905: "windy",
   906: "hail",
   957: "strong-wind",
-  "day-200": "day-thunderstorm",
-  "day-201": "day-thunderstorm",
-  "day-202": "day-thunderstorm",
-  "day-210": "day-lightning",
-  "day-211": "day-lightning",
-  "day-212": "day-lightning",
-  "day-221": "day-lightning",
-  "day-230": "day-thunderstorm",
-  "day-231": "day-thunderstorm",
-  "day-232": "day-thunderstorm",
-  "day-300": "day-sprinkle",
-  "day-301": "day-sprinkle",
-  "day-302": "day-rain",
-  "day-310": "day-rain",
-  "day-311": "day-rain",
-  "day-312": "day-rain",
-  "day-313": "day-rain",
-  "day-314": "day-rain",
-  "day-321": "day-sprinkle",
-  "day-500": "day-sprinkle",
-  "day-501": "day-rain",
-  "day-502": "day-rain",
-  "day-503": "day-rain",
-  "day-504": "day-rain",
-  "day-511": "day-rain-mix",
-  "day-520": "day-showers",
-  "day-521": "day-showers",
-  "day-522": "day-showers",
-  "day-531": "day-storm-showers",
-  "day-600": "day-snow",
-  "day-601": "day-sleet",
-  "day-602": "day-snow",
-  "day-611": "day-rain-mix",
-  "day-612": "day-rain-mix",
-  "day-615": "day-rain-mix",
-  "day-616": "day-rain-mix",
-  "day-620": "day-rain-mix",
-  "day-621": "day-snow",
-  "day-622": "day-snow",
-  "day-701": "day-showers",
-  "day-711": "smoke",
-  "day-721": "day-haze",
-  "day-731": "dust",
-  "day-741": "day-fog",
-  "day-761": "dust",
-  "day-762": "dust",
-  "day-781": "tornado",
-  "day-800": "day-sunny",
-  "day-801": "day-cloudy-gusts",
-  "day-802": "day-cloudy-gusts",
-  "day-803": "day-cloudy-gusts",
-  "day-804": "day-sunny-overcast",
-  "day-900": "tornado",
-  "day-902": "hurricane",
-  "day-903": "snowflake-cold",
-  "day-904": "hot",
-  "day-906": "day-hail",
-  "day-957": "strong-wind",
-  "night-200": "night-alt-thunderstorm",
-  "night-201": "night-alt-thunderstorm",
-  "night-202": "night-alt-thunderstorm",
-  "night-210": "night-alt-lightning",
-  "night-211": "night-alt-lightning",
-  "night-212": "night-alt-lightning",
-  "night-221": "night-alt-lightning",
-  "night-230": "night-alt-thunderstorm",
-  "night-231": "night-alt-thunderstorm",
-  "night-232": "night-alt-thunderstorm",
-  "night-300": "night-alt-sprinkle",
-  "night-301": "night-alt-sprinkle",
-  "night-302": "night-alt-rain",
-  "night-310": "night-alt-rain",
-  "night-311": "night-alt-rain",
-  "night-312": "night-alt-rain",
-  "night-313": "night-alt-rain",
-  "night-314": "night-alt-rain",
-  "night-321": "night-alt-sprinkle",
-  "night-500": "night-alt-sprinkle",
-  "night-501": "night-alt-rain",
-  "night-502": "night-alt-rain",
-  "night-503": "night-alt-rain",
-  "night-504": "night-alt-rain",
-  "night-511": "night-alt-rain-mix",
-  "night-520": "night-alt-showers",
-  "night-521": "night-alt-showers",
-  "night-522": "night-alt-showers",
-  "night-531": "night-alt-storm-showers",
-  "night-600": "night-alt-snow",
-  "night-601": "night-alt-sleet",
-  "night-602": "night-alt-snow",
-  "night-611": "night-alt-rain-mix",
-  "night-612": "night-alt-rain-mix",
-  "night-615": "night-alt-rain-mix",
-  "night-616": "night-alt-rain-mix",
-  "night-620": "night-alt-rain-mix",
-  "night-621": "night-alt-snow",
-  "night-622": "night-alt-snow",
-  "night-701": "night-alt-showers",
-  "night-711": "smoke",
-  "night-721": "day-haze",
-  "night-731": "dust",
-  "night-741": "night-fog",
-  "night-761": "dust",
-  "night-762": "dust",
-  "night-781": "tornado",
-  "night-800": "night-clear",
-  "night-801": "night-alt-cloudy-gusts",
-  "night-802": "night-alt-cloudy-gusts",
-  "night-803": "night-alt-cloudy-gusts",
-  "night-804": "night-alt-cloudy",
-  "night-900": "tornado",
-  "night-902": "hurricane",
-  "night-903": "snowflake-cold",
-  "night-904": "hot",
-  "night-906": "night-alt-hail",
-  "night-957": "strong-wind",
 };
 
 function toggleButton(toggle_button) {
@@ -206,6 +90,8 @@ function toggleButton(toggle_button) {
 }
 
 //Loading animation
+//Function taken from https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+// Don't really get the math but it works, basically will have to pass in a number, "number", with the first range of numbers (0 - 100) and another range of numbers (1 - 0 for opacity )
 function scale(number, loadMin, loadMax, opacityMin, opacityMax) {
   return (
     ((number - loadMin) * (opacityMax - opacityMin)) / (loadMax - loadMin) +
@@ -214,7 +100,6 @@ function scale(number, loadMin, loadMax, opacityMin, opacityMax) {
 }
 
 let load = 0;
-
 let initialize = setInterval(blurring, 15);
 
 function blurring() {
@@ -496,35 +381,3 @@ document.querySelector("#search").addEventListener("click", function () {
 });
 
 // Not useful to use geolocation to get user location if only interested in getting country, seems like pinging it from IP is sufficient
-// let userCoords = {};
-// function successGeolocation(pos) {
-//   let coords = pos.coords;
-//   //coords is an object that is passed back by the getCurrentPosition method
-
-//   userCoords["latitude"] = coords["latitude"];
-//   userCoords["longitude"] = coords["longitude"];
-//   console.log(userCoords);
-//   console.log(coords);
-//   return userCoords;
-// }
-
-// function errorLocation() {
-//   alert("Please allow access to location functions!");
-//   document.querySelector("#geolocation").innerText =
-//     "Location function not active ";
-// }
-// navigator.geolocation.getCurrentPosition(successGeolocation, errorLocation);
-
-// //Getting user location
-// // let user_location = document.querySelector("#user_current_location");
-// // function geoLocation() {
-// //   if (navigator.geolocation) {
-// //     navigator.geolocation.getCurrentPosition(showPosition);
-// //   } else {
-// //     alert("Geolocation is not supported, please enable location services!");
-// //   }
-// // }
-
-// // function showPosition(position) {
-// //   user_location.innerHTML = `Latitude: ${position.coords.latitude} \nLongitude: ${position.coords.longitude}`;
-// // }
